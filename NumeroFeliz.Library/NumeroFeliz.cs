58db28ddd682b8b46f40e36722436e732b00d86e
Cleyde Marlyse de Andrade <cleyde.andrade@gmail.com>
2018-02-03 20:57:01 -0200

    Versão 1.0.0
    adicionei primeiros casos de teste, falta colocar mais de acordo com a especificação do problema

---
+++ NumeroFeliz.Library/NumeroFeliz.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroFeliz.Library
+{
+    public class NumeroFeliz
+    {
+       
+        public static List<int> GerarListaNumeros()
+        {
+            List<int> lista = new List<int>();
+            for (int i = 0; i < 100; i++)
+                if (ComprovarNumero(i)) lista.Add(i);
+            return lista;
+        }
+
+        public static bool ComprovarNumero(int numero)
+        {
+            bool numeroFeliz = false;
+            List<int> listaDigitos = new List<int>();
+            listaDigitos = DividirDigitos(numero);
+            for (int i = 0; i < 20 && !numeroFeliz; i++)
+            {
+                int sumaActual = SomarQuadrados(listaDigitos); 
+                if (sumaActual != 1)
+                    listaDigitos = DividirDigitos(sumaActual);
+                else numeroFeliz = true;
+            }
+            return numeroFeliz;
+        }
+
+
+      
+        public static List<int> DividirDigitos(int digito)
+        {
+            List<int> digitos = new List<int>();
+            while (digito != 0)
+            {
+                digitos.Add(digito % 10); 
+                digito = digito / 10;
+            }
+            return digitos;
+        }
+
+       
+        public static int SomarQuadrados(List<int> listaDigitos)
+        {
+            int resultado = 0;
+            foreach (int elem in listaDigitos) resultado += elem * elem;
+            return resultado;
+        }
+
+        
+
+
+
+    }
+}
+
---
+++ UnitTestNumeroFelizes/NumeroFelizesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumeroFeliz.Library;
+
+namespace UnitTestNumeroFelizes
+{
+    [TestClass]
+    public class NumeroFelizesTests
+    {
+        [TestMethod]
+        public void ComprobarNumeroTest()
+        {
+            Assert.AreEqual(true, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(1));
+            Assert.AreEqual(false, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(2));
+            Assert.AreEqual(false, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(3));
+            Assert.AreEqual(true, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(68));
+            Assert.AreEqual(false, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(69));
+            Assert.AreEqual(true, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(208));
+            Assert.AreEqual(true, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(329));
+            Assert.AreEqual(false, NumeroFeliz.Library.NumeroFeliz.ComprovarNumero(500));
+        }
+    }
+}
